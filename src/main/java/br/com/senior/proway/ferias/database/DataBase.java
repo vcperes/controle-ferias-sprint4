7069ff0e4b33a98f950c02bf7efa2aa4a22e4c61
LorranPdS <dev.danilira@gmail.com>
2021-04-30 10:32:58 -0300

    Apagamos as variavies criadas para praticar Git

--- src/main/java/br/com/senior/proway/ferias/database/DataBase.java
+++ src/main/java/br/com/senior/proway/ferias/database/DataBase.java
@@ -7,8 +7,7 @@ import br.com.senior.proway.ferias.model.SaldoFerias;
 import br.com.senior.proway.ferias.model.interfaces.IFerias;
 
 public final class DataBase {
-	private String nome = "praticandoGit";
-	private String nome1 = "praticandoGit2";
+	
 	private static DataBase instance;
 	public ArrayList<IFerias> ferias;
 	public ArrayList<SaldoFerias> saldoDeFerias;
