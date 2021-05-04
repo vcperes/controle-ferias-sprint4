8852eb30cee1f595d3bceddb26b83cb51a20aad4
Janaina Mai <janainamai@hotmail.com>
2021-05-04 10:47:43 -0300

    Método e testes prontos: cadastrar e deletar.

--- src/test/java/ferias/TesteFeriasDAO.java
+++ src/test/java/ferias/TesteFeriasDAO.java
@@ -80,7 +80,6 @@ public class TesteFeriasDAO {
 
 	@Test
 	public void testPegarPorID() throws SQLException {
-		PostgresConector.conectar();
 		String inserir = "INSERT INTO ferias (id_colaborador, datainicio, datafim, diasvendidos, id_tipoferias) VALUES(1, '03/05/2021', '13/05/2021', 0, 3)";
 		PostgresConector.executarUpdateQuery(inserir);
 		
@@ -94,56 +93,31 @@ public class TesteFeriasDAO {
 		IFerias ferias = feriasDAO.pegarPorID(id);
 		assertEquals(id, ferias.getId());
 		assertEquals(3, ferias.getTipo().getValor());
+		
+		String deletar = "DELETE FROM ferias WHERE id = " + id;
+		PostgresConector.executarUpdateQuery(deletar);
 	}
 
 	@Test
-	public void testCreate() {
-
-		FeriasDirector feriasDiretor = new FeriasDirector();
-		FeriasBuilder feriasBuilder = new FeriasBuilder();
-		// parcial
-		short creditos0 = 29;
-		LocalDate inicio0 = LocalDate.of(2021, 04, 01);
-		LocalDate fim0 = LocalDate.of(2021, 04, 27);
-		feriasDiretor.createFeriasParcial(feriasBuilder, inicio0, fim0, creditos0);
-		Ferias ferias0 = feriasBuilder.build(creditos0);
-		DataBase.getInstance().ferias.add(ferias0);
-		// total
-		short creditos1 = 30;
-		LocalDate inicio1 = LocalDate.of(2021, 04, 01);
-		LocalDate fim1 = LocalDate.of(2021, 04, 30);
-		feriasDiretor.createFeriasTotal(feriasBuilder, inicio1, fim1);
-		Ferias ferias1 = feriasBuilder.build(creditos1);
-		DataBase.getInstance().ferias.add(ferias1);
-		// parcial
-		short creditos2 = 15;
-		LocalDate inicio2 = LocalDate.of(2021, 04, 01);
-		LocalDate fim2 = LocalDate.of(2021, 04, 10);
-		feriasDiretor.createFeriasParcial(feriasBuilder, inicio2, fim2, creditos2);
-		Ferias ferias2 = feriasBuilder.build(creditos2);
-		DataBase.getInstance().ferias.add(ferias2);
-		// total
-		short creditos3 = 20;
-		LocalDate inicio3 = LocalDate.of(2021, 04, 01);
-		LocalDate fim3 = LocalDate.of(2021, 04, 19);
-		feriasDiretor.createFeriasTotal(feriasBuilder, inicio3, fim3);
-		Ferias ferias3 = feriasBuilder.build(creditos3);
-		DataBase.getInstance().ferias.add(ferias3);
-		// invalida
-		short creditos4 = 0;
-		LocalDate inicio4 = LocalDate.of(2021, 04, 05);
-		LocalDate fim4 = LocalDate.of(2021, 04, 01);
-		feriasDiretor.createFeriasParcial(feriasBuilder, inicio4, fim4, creditos4);
-		Ferias ferias4 = feriasBuilder.build(creditos4);
-		DataBase.getInstance().ferias.add(ferias4);
-
-		for (Ferias umaFerias : ferias) {
-			DataBase.getInstance().ferias.add(umaFerias);
-
-			ArrayList<IFerias> ferias = DataBase.getInstance().getFerias();
-			assertEquals(4, ferias.size());
-			DataBase.getInstance().limparListaDeFerias();
+	public void testCadastrar() throws SQLException {
+		IFerias ferias = new Ferias();
+		ferias.setIdentificadorUsuario("1");
+		ferias.setDataInicio(LocalDate.of(2021, 04, 01));
+		ferias.setDataFim(LocalDate.of(2021, 04, 10));
+		ferias.setDiasVendidos((short) 0);
+		ferias.setTipo(TiposFerias.PARCIAL);
+		boolean sucesso = feriasDAO.cadastrar(ferias);
+		assertTrue(sucesso);
+		
+		String pegarID = "SELECT * FROM ferias WHERE id_colaborador = 1 AND diasvendidos = 0 AND id_tipoferias = 3";
+		ResultSet resultSet = PostgresConector.executarQuery(pegarID);
+		int id = 0;
+		if(resultSet.next()) {
+			id = resultSet.getInt("id");
 		}
+		
+		String deletar = "DELETE FROM ferias WHERE id = " + id;
+		PostgresConector.executarUpdateQuery(deletar);
 	}
 
 	@Test
@@ -201,12 +175,21 @@ public class TesteFeriasDAO {
 		}
 	}
 
-//	@Ignore
-//	public void testDelete() {
-// 		this.popularListaDeFerias();
-// 		ArrayList<IFerias> ferias = DataBase.getInstance().ferias;
-// falta implementar m�todo para inserir id no objeto no momento na cria��o
-//	}
+	@Test
+	public void testDelete() throws SQLException {
+		String inserir = "INSERT INTO ferias (id_colaborador, datainicio, datafim, diasvendidos, id_tipoferias) VALUES(1, '03/05/2021', '13/05/2021', 0, 3)";
+		PostgresConector.executarUpdateQuery(inserir);
+		
+		String consultar = "SELECT * FROM ferias WHERE id_colaborador = 1 AND datafim = '13/05/2021'";
+		ResultSet resultSet = PostgresConector.executarQuery(consultar);
+		int id = 0;
+		if(resultSet.next()) {
+			id = resultSet.getInt("id");
+		}
+		
+		boolean sucesso = feriasDAO.deletar(id);
+		assertTrue(sucesso);
+	}
 
 	@Test
 	public void testPegarTodasAsFeriasTotais() {
