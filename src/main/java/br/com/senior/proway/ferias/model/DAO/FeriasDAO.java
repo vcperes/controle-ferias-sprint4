bbbdee77d782df166965c22a299747816602d1ed
Jonata Cardoso Caetano <jonatacaetano88@gmail.com>
2021-05-04 16:07:21 -0300

    Metodos receberam query e teste bem como edicao da documentação

--- src/main/java/br/com/senior/proway/ferias/model/DAO/FeriasDAO.java
+++ src/main/java/br/com/senior/proway/ferias/model/DAO/FeriasDAO.java
@@ -1,10 +1,13 @@
 package br.com.senior.proway.ferias.model.DAO;
 
+import static org.junit.Assert.fail;
+
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+
 import br.com.senior.proway.ferias.database.DataBase;
 import br.com.senior.proway.ferias.model.Ferias;
 import br.com.senior.proway.ferias.model.enums.TiposFerias;
@@ -13,10 +16,15 @@ import br.com.senior.proway.ferias.postgresql.PostgresConector;
 
 public class FeriasDAO implements Icrud<IFerias>, IConsultaDeFeriasPorTipoDAO, IConsultaPorColaboradorDAO {
 
-	/***
-	 * Retorna uma lista com objetos do tipo IFerias.
+	/**
 	 * 
-	 * @return ArrayList<IFerias> Lista de objetos do tipo IFerias.
+	 * Metodo que realiza a busca de todas as ferias existentes no banco de dados.
+	 * 
+	 * Posteriormente armazena as ferias em uma lista (listaFerias) retornando os dados obtidos.  
+	 * 
+	 * @author Jonata Cardoso Caetano.
+	 * @return listaFerias.
+	 * 
 	 */
 	public ArrayList<IFerias> pegarTodos() {
 		ArrayList<IFerias> listaFerias = new ArrayList<IFerias>();
@@ -38,9 +46,9 @@ public class FeriasDAO implements Icrud<IFerias>, IConsultaDeFeriasPorTipoDAO, I
 
 				LocalDate localDateFim = resultSet.getDate("dataFim").toLocalDate();
 				ferias.setDataInicio(localDateFim);
-		
+
 				ferias.setTipo(TiposFerias.pegarPorValor(resultSet.getInt("id_tipoferias")));
-				
+
 				listaFerias.add(ferias);
 			}
 
@@ -98,7 +106,6 @@ public class FeriasDAO implements Icrud<IFerias>, IConsultaDeFeriasPorTipoDAO, I
 	 * @author Janaina
 	 */
 
-
 	public boolean cadastrar(IFerias _ferias) {
 		IFerias ferias = _ferias;
 		boolean cadastrou = false;
@@ -128,23 +135,26 @@ public class FeriasDAO implements Icrud<IFerias>, IConsultaDeFeriasPorTipoDAO, I
 	public boolean alterar(int id, IFerias novaFerias) {
 		boolean sucesso = false;
 		try {
-		PostgresConector.conectar();
-		String query = "UPDATE ferias SET datainicio = '" + Date.valueOf(novaFerias.getDataInicio()) + "', datafim = '" + Date.valueOf(novaFerias.getDataFim()) + "', diasvendidos = " + novaFerias.getDiasVendidos() + ", id_tipoferias = " + novaFerias.getTipo().getValor() + " WHERE id = " + id;
-		PostgresConector.executarUpdateQuery(query);
-		sucesso = true;
-		}
-		catch (SQLException e) {
+			PostgresConector.conectar();
+			String query = "UPDATE ferias SET datainicio = '" + Date.valueOf(novaFerias.getDataInicio())
+					+ "', datafim = '" + Date.valueOf(novaFerias.getDataFim()) + "', diasvendidos = "
+					+ novaFerias.getDiasVendidos() + ", id_tipoferias = " + novaFerias.getTipo().getValor()
+					+ " WHERE id = " + id;
+			PostgresConector.executarUpdateQuery(query);
+			sucesso = true;
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return sucesso;
 	}
 
 	/**
-	 * Deleta objeto do tipo IFerias atraves onde o id é igual ao id passado no parâmetro.
+	 * Deleta objeto do tipo IFerias atraves onde o id é igual ao id passado no
+	 * parâmetro.
 	 * 
 	 * @param int Id Id do objeto a ser deletado.
 	 * @return boolean Retorna se o metodo foi executado com sucesso.
-	 * @author Janaina 
+	 * @author Janaina
 	 */
 	public boolean deletar(int id) {
 		boolean sucesso = false;
@@ -159,11 +169,17 @@ public class FeriasDAO implements Icrud<IFerias>, IConsultaDeFeriasPorTipoDAO, I
 		return sucesso;
 	}
 
-	/***
-	 * Retorna uma lista de Ferias Totais. Retorna uma lista com objetos do tipo
-	 * IFerias onde o TipoDeFerias eh igual a TOTAL.
+	
+	/**
 	 * 
-	 * @return ArrayList<IFerias> Lista de objetos do tipo IFerias.
+	 * Metodo que realiza a busca de todas as ferias do tipo TOTAL existentes no banco de dados.
+	 * 
+	 * Posteriormente armazena as ferias em uma lista (listaTodasAsFeriasTotais) retornando os dados obtidos.  
+	 * 
+	 * @author Jonata Cardoso Caetano.
+	 * 
+	 * @return listaTodasAsFeriasParciais.
+	 * 
 	 */
 	public ArrayList<IFerias> pegarTodasAsFeriasTotais() {
 		ArrayList<IFerias> listaTodasAsFeriasTotais = new ArrayList<IFerias>();
@@ -193,6 +209,7 @@ public class FeriasDAO implements Icrud<IFerias>, IConsultaDeFeriasPorTipoDAO, I
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			fail(e.getMessage());
 		}
 		return listaTodasAsFeriasTotais;
 	}
@@ -214,21 +231,49 @@ public class FeriasDAO implements Icrud<IFerias>, IConsultaDeFeriasPorTipoDAO, I
 		return feriasInvalidas;
 	}
 
-	/***
-	 * Retorna uma lista de Ferias Parciais. Retorna uma lista com objetos do tipo
-	 * IFerias onde o TipoDeFerias eh igual a PARCIAL.
+	/**
 	 * 
-	 * @return ArrayList<IFerias> Lista de objetos do tipo IFerias.
+	 * Metodo que realiza a busca de todas as ferias do tipo Parcial existentes no banco de dados.
+	 * 
+	 * Posteriormente armazena as ferias em uma lista (listaTodasAsFeriasParciais) retornando os dados obtidos.  
+	 * 
+	 * @author Jonata Cardoso Caetano.
+	 * 
+	 * @return listaTodasAsFeriasParciais.
+	 * 
 	 */
 	public ArrayList<IFerias> pegarTodasAsFeriasParciais() {
-		ArrayList<IFerias> ferias = DataBase.getInstance().getFerias();
-		ArrayList<IFerias> feriasParciais = new ArrayList<IFerias>();
-		for (IFerias umaFerias : ferias) {
-			if (umaFerias.getTipo().equals(TiposFerias.PARCIAL)) {
-				feriasParciais.add(umaFerias);
+		ArrayList<IFerias> listaTodasAsFeriasParciais = new ArrayList<IFerias>();
+
+		try {
+			PostgresConector.conectar();
+			String query = "SELECT *FROM ferias WHERE id_tipoferias = 3";
+			ResultSet resultSet = PostgresConector.executarQuery(query);
+
+			while (resultSet.next()) {
+				Ferias ferias2 = new Ferias();
+
+				ferias2.setId(resultSet.getInt("id"));
+				ferias2.setIdentificadorUsuario(resultSet.getString("id_colaborador"));
+
+				LocalDate localDataInicio = resultSet.getDate("DataInicio").toLocalDate();
+				ferias2.setDataInicio(localDataInicio);
+
+				LocalDate localDataFim = resultSet.getDate("DataFim").toLocalDate();
+				ferias2.setDataFim(localDataFim);
+
+				ferias2.setDiasVendidos((short) resultSet.getInt("diasvendidos"));
+
+				ferias2.setTipo(TiposFerias.pegarPorValor(resultSet.getInt("id_tipoferias")));
+
+				listaTodasAsFeriasParciais.add(ferias2);
 			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return feriasParciais;
+		return listaTodasAsFeriasParciais;
+
 	}
 
 	/***
--- src/test/java/ferias/TesteFeriasDAO.java
+++ src/test/java/ferias/TesteFeriasDAO.java
@@ -1,12 +1,11 @@
 package ferias;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.junit.Test;
 
@@ -39,7 +38,6 @@ public class TesteFeriasDAO {
 		PostgresConector.executarUpdateQuery(deletarId1);
 		String deletarId2 = "DELETE FROM ferias WHERE id_colaborador = 2 and datainicio = '03/05/2021' and datafim = '13/06/2021' and diasvendidos = 0 and id_tipoferias = 3";
 		PostgresConector.executarUpdateQuery(deletarId2);
-
 	}
 
 	@Test
@@ -110,7 +108,7 @@ public class TesteFeriasDAO {
 		ferias.setDiasVendidos((short) 0);
 		ferias.setTipo(TiposFerias.PARCIAL);
 		boolean sucesso = feriasDAO.alterar(id, ferias);
-		
+
 		assertTrue(sucesso);
 	}
 
@@ -131,32 +129,28 @@ public class TesteFeriasDAO {
 	}
 
 	@Test
-	public void testPegarTodasAsFeriasTotais() throws SQLException{
-		
+	public void testPegarTodasAsFeriasTotais() throws SQLException {
+
 		String inserir = "INSERT INTO ferias (id_colaborador, datainicio, datafim, diasvendidos, id_tipoferias) VALUES(1, '03/05/2021', '13/06/2021', 0, 2)";
 		PostgresConector.executarUpdateQuery(inserir);
 		String inserir2 = "INSERT INTO ferias (id_colaborador, datainicio, datafim, diasvendidos, id_tipoferias) VALUES(2, '03/05/2021', '13/06/2021', 0, 2)";
 		PostgresConector.executarUpdateQuery(inserir2);
-		
+
 		ArrayList<IFerias> listaTodasAsFeriasTotais = feriasDAO.pegarTodasAsFeriasTotais();
 		boolean ehFeriasTotal = true;
 		for (IFerias iFerias : listaTodasAsFeriasTotais) {
-			if (iFerias.getTipo().getValor()!=2) {
+			if (iFerias.getTipo().getValor() != 2) {
 				ehFeriasTotal = false;
 			}
-			
 			assertTrue(ehFeriasTotal);
-			
 		}
-		
-		
+
 		String deletarId1 = "DELETE FROM ferias WHERE id_colaborador = 1 and datainicio = '03/05/2021' and datafim = '13/06/2021' and diasvendidos = 0 and id_tipoferias = 2";
 		PostgresConector.executarUpdateQuery(deletarId1);
 		String deletarId2 = "DELETE FROM ferias WHERE id_colaborador = 2 and datainicio = '03/05/2021' and datafim = '13/06/2021' and diasvendidos = 0 and id_tipoferias = 2";
 		PostgresConector.executarUpdateQuery(deletarId2);
 	}
 
-		
 	@Test
 	public void testPegarTodasAsFeriasInvalidas() {
 
@@ -208,68 +202,27 @@ public class TesteFeriasDAO {
 	}
 
 	@Test
-	public void testPegarTodasAsFeriasParciais() {
+	public void testPegarTodasAsFeriasParciais() throws SQLException {
 
-		FeriasDirector feriasDiretor = new FeriasDirector();
-		FeriasBuilder feriasBuilder = new FeriasBuilder();
-		// parcial
-		short creditos0 = 29;
-		LocalDate inicio0 = LocalDate.of(2021, 04, 01);
-		LocalDate fim0 = LocalDate.of(2021, 04, 27);
-		feriasDiretor.createFeriasParcial(feriasBuilder, inicio0, fim0, creditos0);
-		Ferias ferias0 = feriasBuilder.build(creditos0);
-		DataBase.getInstance().ferias.add(ferias0);
-		// total
-		short creditos1 = 30;
-		LocalDate inicio1 = LocalDate.of(2021, 04, 01);
-		LocalDate fim1 = LocalDate.of(2021, 04, 30);
-		feriasDiretor.createFeriasTotal(feriasBuilder, inicio1, fim1);
-		Ferias ferias1 = feriasBuilder.build(creditos1);
-		DataBase.getInstance().ferias.add(ferias1);
-		// parcial
-		short creditos2 = 15;
-		LocalDate inicio2 = LocalDate.of(2021, 04, 01);
-		LocalDate fim2 = LocalDate.of(2021, 04, 10);
-		feriasDiretor.createFeriasParcial(feriasBuilder, inicio2, fim2, creditos2);
-		Ferias ferias2 = feriasBuilder.build(creditos2);
-		DataBase.getInstance().ferias.add(ferias2);
-		// total
-		short creditos3 = 20;
-		LocalDate inicio3 = LocalDate.of(2021, 04, 01);
-		LocalDate fim3 = LocalDate.of(2021, 04, 19);
-		feriasDiretor.createFeriasTotal(feriasBuilder, inicio3, fim3);
-		Ferias ferias3 = feriasBuilder.build(creditos3);
-		DataBase.getInstance().ferias.add(ferias3);
-		// invalida
-		short creditos4 = 0;
-		LocalDate inicio4 = LocalDate.of(2021, 04, 05);
-		LocalDate fim4 = LocalDate.of(2021, 04, 01);
-		feriasDiretor.createFeriasParcial(feriasBuilder, inicio4, fim4, creditos4);
-		Ferias ferias4 = feriasBuilder.build(creditos4);
-		DataBase.getInstance().ferias.add(ferias4);
+		String inserir = "INSERT INTO ferias (id_colaborador, datainicio, datafim, diasvendidos, id_tipoferias) VALUES(3, '03/05/2021', '13/06/2021', 0, 3)";
+		PostgresConector.executarUpdateQuery(inserir);
+		String inserir2 = "INSERT INTO ferias (id_colaborador, datainicio, datafim, diasvendidos, id_tipoferias) VALUES(4, '03/05/2021', '13/06/2021', 0, 3)";
+		PostgresConector.executarUpdateQuery(inserir2);
 
-		for (Ferias umaFerias : ferias) {
-			DataBase.getInstance().ferias.add(umaFerias);
+		ArrayList<IFerias> listaTodasAsFeriasParciais = feriasDAO.pegarTodasAsFeriasParciais();
+		boolean ehFeriasParcial = true;
+		for (IFerias iFerias2 : listaTodasAsFeriasParciais) {
 
-			ArrayList<IFerias> ferias = feriasDAO.pegarTodasAsFeriasParciais();
-			assertEquals(2, ferias.size());
-			DataBase.getInstance().limparListaDeFerias();
+			if (iFerias2.getTipo().getValor() != 3) {
+				ehFeriasParcial = false;
+
+			}
+			assertTrue(ehFeriasParcial);
 		}
 
+		String deletarId1 = "DELETE FROM ferias WHERE id_colaborador = 3 and datainicio = '03/05/2021' and datafim = '13/06/2021' and diasvendidos = 0 and id_tipoferias = 3";
+		PostgresConector.executarUpdateQuery(deletarId1);
+		String deletarId2 = "DELETE FROM ferias WHERE id_colaborador = 4 and datainicio = '03/05/2021' and datafim = '13/06/2021' and diasvendidos = 0 and id_tipoferias = 3";
+		PostgresConector.executarUpdateQuery(deletarId2);
 	}
-
-//	@Ignore
-//	public void testPegarTodasAsFeriasFracionadas() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Ignore
-//	public void testPegarTodasAsFeriasVendidas() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Ignore
-//	public void testPegarFeriasPorIDColaborador() {
-//		fail("Not yet implemented");
-//	}
 }
